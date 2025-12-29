d8fcaf4660c495c61d35b335f35daa63f5e13e37
Ben <meowcat@meowcat.site>
2025-12-29 10:51:11 +0000

    update gitignore

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Starry Systems and Nijika Softworks.
+ */
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <p>© 2025 Nijika Softworks & Neuron Technologies</p>
+            </div>
+        </footer>
+    );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Starry Systems and Nijika Softworks.
+ */
+
+export default function Navbar() {
+    return (
+        <div className="topnav">
+            <a href="/">Home</a>
+            <a href="/games">Games</a>
+            <a href="/opensource">Source Code</a>
+            <a href="/changelog">Changelog</a>
+            <a href="/archive">Archive</a>
+            <a href="https://neurontechnologies.flarum.cloud/d/3-space-cat-games-general">
+                Help
+            </a>
+
+        </div>
+    );
+}
