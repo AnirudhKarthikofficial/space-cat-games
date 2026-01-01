84a0e8c3821517e5125a50df04cea8abc2e433e4
Ben <meowcat@meowcat.site>
2026-01-01 14:59:38 +0000

    chore: clean up credits

--- src/credits.jsx
+++ src/credits.jsx
@@ -30,42 +30,6 @@ export default function Credits() {
                     </ul>
                 </section>
                 <section className="credits-section">
-                    <h2>Testers</h2>
-                    <ul className="credits-list">
-                        <li>
-                            <strong>Jake</strong>
-                        </li>
-                        <li>
-                            <strong>Jayden</strong>
-                        </li>
-                        <li>
-                            <strong>Luka</strong>
-                        </li>
-                    </ul>
-                </section>
-                <section className="credits-section">
-                    <h2>Nijika Softworks</h2>
-                    <p>
-                        Space Cat Games is now owned and operated by Nijika Softworks, a VN and
-                        RPG development group.
-                    </p>
-                    <h3>Developers:</h3>
-                    <ul className="credits-list">
-                        <li>
-                            <strong>Meowcat</strong> (been here since the beginning)
-                        </li>
-                    </ul>
-                    <h3>Server Moderators:</h3>
-                    <ul className="credits-list">
-                        <li>Meowcat</li>
-                    </ul>
-                    <div className="cta-section">
-                        <a href="https://discord.gg/8hx2hnGPhH" className="btn">
-                            Join our Discord Server
-                        </a>
-                    </div>
-                </section>
-                <section className="credits-section">
                     <h2>Special Thanks</h2>
                     <ul className="credits-list">
                         <li>
@@ -80,77 +44,23 @@ export default function Credits() {
                         <li>
                             <strong>Netlify:</strong> For keeping us running at the moment.
                         </li>
-                        <li>
-                            <strong>npm: </strong>For being the JavaScript package manager we used
-                            for almost a year. {" "}
-                        </li>
                     </ul>
                 </section>
-                <section className="credits-section">
-                    <h2>Game/Tools Credits</h2>
-                    <p>
-                        The games featured on Space Cat Games come from various sources,
-                        developers, and open-source projects. We want to acknowledge and thank
-                        all the creators who make these games and tools possible.
-                    </p>
-                    <ul className="credits-list">
-                        <li>
-                            <strong>Tetris:</strong> Original concept by Alexey Pajitnov. Web
-                            implementation by various developers.
-                        </li>
-                        <li>
-                            <strong>Chrome Dino:</strong> Originally developed by Google as an
-                            offline game for Chrome browser.
-                        </li>
-                        <li>
-                            <strong>Geometry Dash:</strong> Created by Robert Topala (RobTop
-                            Games). Web version adapted by various developers.
-                        </li>
-                        <li>
-                            <strong>Minecraft:</strong> Created by Markus Persson and later
-                            developed by Mojang Studios. Web version is an unofficial adaptation.
-                        </li>
-                        <li>
-                            <strong>Doodle Jump:</strong> Originally developed by Lima Sky. Web
-                            adaptation by various developers.
-                        </li>
-                        <li>
-                            <strong>Minesweeper:</strong> Classic Windows game, adapted for web by
-                            various developers.
-                        </li>
-                        <li>
-                            <strong>Paper.io 2:</strong> Developed by Voodoo. Web version adapted
-                            for educational purposes.
-                        </li>
-                        <li>
-                            <strong>Wordle:</strong> Created by Josh Wardle, adapted for web by
-                            various developers.
-                        </li>
-                        <li>
-                            <strong>Chicken Game 2:</strong> Created by Chase, packaged with the
-                            Ikuyo Packaging System
-                        </li>
-                    </ul>
                     <h3>Tools</h3>
                     <ul className="credits-list">
                         <li>
-                            <strong>Ikuyo Packaging System: </strong>Forked from Turbowarp,
-                            modifed for Nijika Libaries by Meowcat.
+                            <strong>React: </strong>Created by Meta Platforms, Inc.{' '}
+                            <a href="https://react.dev/">https://react.dev</a>
                         </li>
                         <li>
-                            <strong>Yarn: </strong>Created by YarnPKG. <a href="https://yarnpkg.com/">https://yarnpkg.com/</a>
+                            <strong>Vite: </strong>Created by VoidZero Inc. <a href="https://vite.dev/">https://vite.dev</a>
                         </li>
                         <li>
-                            <strong>Visual Studio Code: </strong>IDE used at Ijichi Systems. <a href="https://code.visualstudio.com/">
-                                https://code.visualstudio.com/
-                            </a>
+                            <strong>Git: </strong>Source Control Management System used at Neuron Technologies <a href="https://git-scm.com/">https://git-scm.com/</a>
                         </li>
                         <li>
-                            <strong>Git: </strong>Source Control Management System used at Starry
-                            Systems. <a href="https://git-scm.com/">https://git-scm.com/</a>
-                        </li>
-                        <li>
-                            <strong>Jekyll: </strong>Static Site Converter. <a href="https://jekyllrb.com/">https://jekyllrb.com/</a>
+                            <strong>Node.js: </strong>The JavaScript runtime we use.{' '}
+                            <a href="https://nodejs.org/">https://nodejs.org/</a>
                         </li>
                         <li>
                             <strong>OwO </strong>Huohhhh. fuwwy stwing twansfowmew ;3
@@ -160,7 +70,6 @@ export default function Credits() {
                             </a>
                         </li>
                     </ul>
-                </section>
             </div>
 
             <Footer />
