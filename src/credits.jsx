973f45c58e233bff70a4c512cfeea2824f3cf351
Ben <meowcat@meowcat.site>
2026-01-01 14:26:10 +0000

    fix: credits component was using footer.tsx without importing

--- src/credits.jsx
+++ src/credits.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 export default function Credits() {
     return (
