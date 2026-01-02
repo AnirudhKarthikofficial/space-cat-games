8774f1de336e97cf5d59ddbadf41b2db63a40b8f
Ben <meowcat@meowcat.site>
2026-01-02 23:11:50 +0000

    make sure we load uptime everytime

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,29 +2,47 @@
  * Copyright (c) Starry Systems and Nijika Softworks.
  */
 
-import { useEffect } from "react";
-
+import { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
 
 export default function Footer() {
+    const location = useLocation()
+    const navigate = useNavigate()
+
     useEffect(() => {
+        // Remove old TickCounter script if exists
+        const existing = document.getElementById("tickcounter-sdk")
+        if (existing) {
+            existing.remove()
+        }
+
         // Load TickCounter script
-        if (document.getElementById("tickcounter-sdk")) return;
-        
-        const script = document.createElement("script");
-        script.id = "tickcounter-sdk";
-        script.src = "//www.tickcounter.com/static/js/loader.js";
-        document.body.appendChild(script);
-    }, []);
+        const script = document.createElement("script")
+        script.id = "tickcounter-sdk"
+        script.src = "//www.tickcounter.com/static/js/loader.js"
+        document.body.appendChild(script)
+    }, [location.pathname]) // re-run on route change
 
     return (
         <footer>
             <div className="container">
-                <p>© 2025 Nijika Softworks & <a href="https://github.com/neurontechofficial">Neuron Technologies </a> | All rights reserved.</p>
-                <img src={"images/react.png"} width={150} height={60}/>    <img src={"images/vite.svg"} height={55}/>
+                <p>
+                    © 2025 Nijika Softworks &{" "}
+                    <a href="https://github.com/neurontechofficial">Neuron Technologies</a>{" "}
+                    | All rights reserved.
+                </p>
+
+                {/* Images */}
+                <img src="images/react.png" width={150} height={60} />
+                <img src="images/vite.svg" height={55} />
                 <br />
+
+                {/* Build Info */}
                 <div>Built on: {new Date(__BUILD_INFO__.date).toLocaleString()}</div>
                 <div>Node: {__BUILD_INFO__.node}</div>
                 <div>Vite: {__BUILD_INFO__.vite}</div>
+
+                {/* TickCounter widget */}
                 <a
                     data-type="countup"
                     data-id="367698"
@@ -43,10 +61,12 @@ export default function Footer() {
                 >
                     Uptime
                 </a>
+
                 <a href="/debug">
                     <button>Debugger</button>
                 </a>
+
             </div>
         </footer>
-    );
+    )
 }
