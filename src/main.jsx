e48bbf58615adb93c9536b4b6ec0daf41a452b3a
Ben <meowcat@meowcat.site>
2026-01-06 16:22:43 +0000

    trying a fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-/*
- * Copyright (c) Starry Systems and Nijika Softworks.
- */
-
 import React from 'react'
-import {createRoot} from 'react-dom/client'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+
 import Home from './index.jsx'
 import Games from './games.jsx'
 import Credits from './credits.jsx'
@@ -15,54 +12,27 @@ import Privacy from './privacy.jsx'
 import DebugDashboard from './components/DebugDashboard'
 
 const rootEl = document.getElementById('root')
-if (rootEl) {
-    const root = createRoot(rootEl)
-    root.render(
-        <BrowserRouter>
-            <Routes>
-                {/* Home */}
-                <Route path="/" element={<Home/>}/>
-                <Route path="/index.html" element={<Home/>}/>
 
-                {/* Games */}
-                <Route path="/games" element={<Games/>}/>
-                <Route path="/games.html" element={<Games/>}/>
-                <Route path="/games/index.html" element={<Games/>}/>
-                <Route path="/games/" element={<Games/>}/>
-
-                {/* Credits */}
-                <Route path="/credits" element={<Credits/>}/>
-                <Route path="/credits.html" element={<Credits/>}/>
-                <Route path="/credits/index.html" element={<Credits/>}/>
-
-                {/* Archive */}
-                <Route path="/archive" element={<Archive/>}/>
-                <Route path="/archive.html" element={<Archive/>}/>
-                <Route path="/archive/index.html" element={<Archive/>}/>
-                <Route path="/archive/" element={<Archive/>}/>
-
-                {/* Opensource */}
-                <Route path="/opensource" element={<Opensource/>}/>
-                <Route path="/opensource.html" element={<Opensource/>}/>
-                <Route path="/opensource/index.html" element={<Opensource/>}/>
-
-                {/* Changelog */}
-                <Route path="/changelog" element={<Changelog/>}/>
-                <Route path="/changelog.html" element={<Changelog/>}/>
-                <Route path="/changelog/index.html" element={<Changelog/>}/>
-
-                {/* Privacy */}
-                <Route path="/privacy" element={<Privacy/>}/>
-                <Route path="/privacy.html" element={<Privacy/>}/>
-                <Route path="/privacy/index.html" element={<Privacy/>}/>
-
-                {/* Debug Dashboard */}
-                <Route path="/debug" element={<DebugDashboard/>}/>
-
-                    <Navigate to="/" replace/>
-            </Routes>
-        </BrowserRouter>
-    )
-} else {
+if (!rootEl) {
     console.warn('Root element not found; cannot mount React app')
+} else {
+    createRoot(rootEl).render(
+        <React.StrictMode>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/games" element={<Games />} />
+                    <Route path="/credits" element={<Credits />} />
+                    <Route path="/archive" element={<Archive />} />
+                    <Route path="/opensource" element={<Opensource />} />
+                    <Route path="/changelog" element={<Changelog />} />
+                    <Route path="/privacy" element={<Privacy />} />
+                    <Route path="/debug" element={<DebugDashboard />} />
+
+                    {/* Catch-all */}
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </HashRouter>
+        </React.StrictMode>
+    )
 }
