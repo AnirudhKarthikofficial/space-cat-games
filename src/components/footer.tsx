de5102149c22dc106407d354ce17dab6158b56e9
Ben House <165931265+meowmeow10@users.noreply.github.com>
2026-01-20 10:34:14 +0000

    version in footer?

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,69 +2,70 @@
  * Copyright (c) Starry Systems and Nijika Softworks.
  */
 
-import { useEffect } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Footer() {
-    const location = useLocation()
-    const navigate = useNavigate()
+  const location = useLocation();
+  const navigate = useNavigate();
+  const version = __BUILD_INFO__.appVersion;
 
-    useEffect(() => {
-        // Remove old TickCounter script if exists
-        const existing = document.getElementById("tickcounter-sdk")
-        if (existing) {
-            existing.remove()
-        }
+  useEffect(() => {
+    // Remove old TickCounter script if exists
+    const existing = document.getElementById("tickcounter-sdk");
+    if (existing) {
+      existing.remove();
+    }
 
-        // Load TickCounter script
-        const script = document.createElement("script")
-        script.id = "tickcounter-sdk"
-        script.src = "//www.tickcounter.com/static/js/loader.js"
-        document.body.appendChild(script)
-    }, [location.pathname]) // re-run on route change
+    // Load TickCounter script
+    const script = document.createElement("script");
+    script.id = "tickcounter-sdk";
+    script.src = "//www.tickcounter.com/static/js/loader.js";
+    document.body.appendChild(script);
+  }, [location.pathname]); // re-run on route change
 
-    return (
-        <footer>
-            <div className="container">
-                <p>
-                    © 2025 Nijika Softworks &{" "}
-                    <a href="https://github.com/neurontechofficial">Neuron Technologies</a>{" "}
-                    | All rights reserved.
-                </p>
+  return (
+    <footer>
+      <div className="container">
+        <p>
+          © 2025 Nijika Softworks &{" "}
+          <a href="https://github.com/neurontechofficial">
+            Neuron Technologies
+          </a>{" "}
+          | All rights reserved.
+        </p>
 
-                {/* Images */}
-                <img src="images/react.png" width={150} height={60} />
-                <img src="images/vite.svg" height={55} />
-                <br />
+        {/* Images */}
+        <img src="images/react.png" width={150} height={60} />
+        <img src="images/vite.svg" height={55} />
+        <br />
 
-                {/* Build Info */}
-                <div>Built on: {new Date(__BUILD_INFO__.date).toLocaleString()}</div>
-                <div>Node: {__BUILD_INFO__.node}</div>
-                <div>Vite: {__BUILD_INFO__.vite}</div>
+        {/* Build Info */}
+        <div>Built on: {new Date(__BUILD_INFO__.date).toLocaleString()}</div>
+        <div>Node: {__BUILD_INFO__.node}</div>
+        <div>Vite: {__BUILD_INFO__.vite}</div>
+        <div>Version: {version}</div>
 
-                {/* TickCounter widget */}
-                <a
-                    data-type="countup"
-                    data-id="367698"
-                    className="tickcounter"
-                    style={{
-                        display: "block",
-                        left: 0,
-                        width: "100%",
-                        height: "50px",
-                        position: "relative",
-                        paddingBottom: 0,
-                        margin: "0 auto",
-                    }}
-                    title="Uptime"
-                    href="//www.tickcounter.com/countup"
-                >
-                    Uptime
-                </a>
-
-
-
-            </div>
-        </footer>
-    )
+        {/* TickCounter widget */}
+        <a
+          data-type="countup"
+          data-id="367698"
+          className="tickcounter"
+          style={{
+            display: "block",
+            left: 0,
+            width: "100%",
+            height: "50px",
+            position: "relative",
+            paddingBottom: 0,
+            margin: "0 auto",
+          }}
+          title="Uptime"
+          href="//www.tickcounter.com/countup"
+        >
+          Uptime
+        </a>
+      </div>
+    </footer>
+  );
 }
